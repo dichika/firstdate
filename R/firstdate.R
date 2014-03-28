f15e6394a66ecfd856c87d0f42779b85a8c6a40b
dichika <ichikawa@hcc-jp.com>
2014-03-28 21:46:07 +0900

    bug fix

--- R/firstdate.R
+++ R/firstdate.R
@@ -1,4 +1,4 @@
-getATND <- function(username=NULL){
+getATNDUsers <- function(username=NULL){
   require(jsonlite)
   require(RCurl)
   if(is.null(username)){
@@ -11,7 +11,7 @@ getATND <- function(username=NULL){
   return(res)
 }
 
-getEventUsers <- function(eventid=NULL){
+getATNDEventUsers <- function(eventid=NULL){
   require(jsonlite)
   require(RCurl)
   if(is.null(eventid)){
@@ -20,7 +20,7 @@ getEventUsers <- function(eventid=NULL){
   url <- "http://api.atnd.org/events/users/?event_id=%s&format=json"
   s <- sprintf(url, eventid)
   res <- getURL(s)
-  res <- fromJSON(res)$event$users[[1]]
+  res <- jsonlite::fromJSON(res)$event$users[[1]]
   return(res)
 }
 
@@ -67,7 +67,7 @@ getTwitter <- function(user, key, secret){
   twitter_token <- oauth1.0_token(oauth_endpoints("twitter"), myapp)
   url <- sprintf("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=%s&count=180&include_rts=1",user)
   req <- GET(url,config(token = twitter_token))
-  resjson <- fromJSON(content(req, as="text"))
+  resjson <- rjson::fromJSON(content(req, as="text"))
   tmp <- Sys.getlocale("LC_TIME")
   Sys.setlocale("LC_TIME","C")
   result <- ldply(resjson, function(x){data.frame(
@@ -75,11 +75,15 @@ getTwitter <- function(user, key, secret){
     screen_name=x$user$screen_name,
     location=x$user$location,
     time=as.POSIXlt(paste0(substr(x$created_at,5,19),substr(x$created_at,27,30)),
-                 format="%B %d %H:%M:%S%Y"),
+                 format="%B %d %H:%M:%S%Y",tz="Europe/London"),
     tweetid=x$id_str,
     text=x$text,
+    media=ifelse(!is.null(x$entities$media[[1]]$media_url),
+                 x$entities$media[[1]]$media_url,
+                 NA),
     stringsAsFactors=FALSE
   )})
+  attributes(result$time)$tzone <- "Japan"
   Sys.setlocale("LC_TIME",tmp)
   invisible(result)
 }
@@ -103,8 +107,8 @@ visTL <- function(tweetdata, group="name", path="sample.html"){
            forceY=paste0("#![",min(tweetdata$tmp)-1, ",", max(tweetdata$tmp)+1, "]!#")
            )
   n1$xAxis(tickFormat = "#!function(d) {return d3.time.format('%m-%d %H')(new Date( d * 1000 ));}!#")
-  n1$save("sample.html", cdn=TRUE)
-  tmp <- paste(readLines(path, warn=FALSE), collapse="\n")
+  n1$save("xxxxxxxxxxxxxxx.html", cdn=TRUE)
+  tmp <- paste(readLines("xxxxxxxxxxxxxxx.html", warn=FALSE), collapse="\n")
   tmp <- gsub("<style>.+</style>","<style>
     .rChart {
       display: block;
@@ -119,5 +123,96 @@ visTL <- function(tweetdata, group="name", path="sample.html"){
 
     </style>", tmp)
   writeLines(tmp, path)
-  browseURL(path)
+}
+
+slideTL <- function(res, covertitle="title", covertext="text", covertime="2014-01-01 00:00:00",path="output.html"){
+  require(rCharts)
+  releaseList <- plyr::alply(res, 1, function(x) {
+    list(
+      #       startDate = format(as.Date(x$time), "%Y,%m,%d"), 
+      startDate = format(x$time, "%Y,%m,%d,%H,%M,%S"), 
+      headline = x$screen_name, 
+      text = x$text,
+      asset=if(!is.na(x$media)){list(media=x$media)}else{list()}
+    )
+  })
+  
+  m = Timeline$new()
+  m$main(headline = covertitle,
+         type = "default", 
+         text = covertext,
+         startDate = format(as.POSIXlt(covertime), "%Y,%m,%d,%H,%M,%S"), 
+#          startDate = format(as.Date(min(res$time)), "%Y,%m,%d"), 
+         asset = list()
+  )
+# m$config(start_zoom_adjust="6")
+  names(releaseList) <- NULL
+  m$event(releaseList)
+  tmp <- tempfile()
+  m$save(tmp)
+  
+  x <- paste(readLines(tmp, warn = F), collapse = "\n")
+  x <- gsub(paste0(system.file(package="rCharts"),"/libraries/timeline/js/storyjs-embed.js"), 
+            "http://cdn.knightlab.com/libs/timeline/2.26.1/js/storyjs-embed.js", 
+            x)
+  writeLines(x, con = path)
+}
+
+getExif <- function(path){
+  require(XML)
+  tmp <- tempfile()
+  cond <- try(url(path),silent=TRUE)
+  if(!any(class(cond) %in% "try-error")){
+    download.file(path, tmp)
+    path <- tmp
+  }
+  path2 <- sprintf('exiftool -h "%s"', path)
+  info <- system(path2, inter=TRUE)
+  info2 <- gsub("<!.+-->","",paste(collapse="", info))
+  result <- readHTMLTable(info2, stringsAsFactors=FALSE)[[1]]
+  result <- cbind(path=path, result)
+  colnames(result) <- c("path", "variable", "value")
+  invisible(result)
+}
+
+getExif2Map <- function(path){
+  require(XML)
+  tmp <- tempfile()
+  cond <- try(url(path),silent=TRUE)
+  if(!any(class(cond) %in% "try-error")){
+    download.file(path, tmp)
+    path <- tmp
+  }
+  path2 <- sprintf('exiftool -h "%s"', path)
+  info <- system(path2, inter=TRUE)
+  info2 <- gsub("<!.+-->","",paste(collapse="", info))
+  result <- readHTMLTable(info2, stringsAsFactors=FALSE)[[1]]
+  colnames(result) <- c("variable","value")
+  if(!any(result$variable %in% "GPS Position")){
+    stop("\nThis photo seems to have no GPS information.")
+  }
+  convLatLon <- function(latlon){
+    res <- strsplit(latlon, split=" ")[[1]]
+    res <- res[grepl("[[:digit:]]",res)]
+    res <- as.numeric(gsub("[[:punct:]]", "",res))
+    res <- res[1] + res[2]/60 + res[3]/360000
+  }
+  lat <- convLatLon(result[result$variable=="GPS Latitude","value"])
+  lon <- convLatLon(result[result$variable=="GPS Longitude","value"])
+  u <- sprintf("https://www.google.com/maps/place/%s,%s", lat, lon)
+  browseURL(u)
+}
+
+
+getHealthGraph <- function(key, secret){
+  require(httr)
+  runkeeper <- oauth_endpoint(base_url="https://runkeeper.com/apps",
+                              authorize="authorize",
+                              access="token")
+  myapp <- oauth_app("runkeeper", key, secret)
+  runkeeper_token <- oauth2.0_token(runkeeper, myapp)
+  req <- GET("http://api.runkeeper.com/user", 
+             config(token = runkeeper_token))
+  resjson <- rjson::fromJSON(content(req, as="text"))
+  resjson
 }
